0e5e050b1958f0c1074444023872a21174559756
YoussefJell <youssef.jellouli@yahoo.com>
2022-08-12 22:01:23 +0000

    add

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills the killmenow process
-exec { 'kill_proc'
+exec { 'kill_proc':
   command => 'pkill -f killmenow',
-  path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
