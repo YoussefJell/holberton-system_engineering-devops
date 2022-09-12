3f55c12e8d4649e82157f8e7546ca3d4d4e99bbd
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-12 14:45:21 +0000

    if_name_main

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,28 +4,30 @@ from json import dumps
 import requests
 from sys import argv
 
-try:
-    int(argv[1])
-except Exception as e:
-    exit(1)
+if __name__ == "__main__":
+    try:
+        int(argv[1])
+    except Exception as e:
+        exit(1)
 
-employeeID = int(argv[1])
-endpoint = "https://jsonplaceholder.typicode.com"
-user = requests.get("{}/users/{}".format(endpoint, employeeID)).json()
-todo = requests.get("{}/users/{}/todos".format(endpoint, employeeID)).json()
-employeeName = user.get('username')
+    employeeID = int(argv[1])
+    endpoint = "https://jsonplaceholder.typicode.com"
+    user = requests.get("{}/users/{}".format(endpoint, employeeID)).json()
+    todo = requests.get(
+        "{}/users/{}/todos".format(endpoint, employeeID)).json()
+    employeeName = user.get('username')
 
-row = []
+    row = []
 
-for elem in todo:
-    dict = {
-        "task": elem.get('title'),
-        "completed": elem.get('completed'),
-        "username": employeeName
-    }
-    row.append(dict)
+    for elem in todo:
+        dict = {
+            "task": elem.get('title'),
+            "completed": elem.get('completed'),
+            "username": employeeName
+        }
+        row.append(dict)
 
-employeeDict = {employeeID: row}
+    employeeDict = {employeeID: row}
 
-with open('{}.json'.format(employeeID), 'w', encoding="utf-8") as file:
-    file.write(dumps(employeeDict))
+    with open('{}.json'.format(employeeID), 'w', encoding="utf-8") as file:
+        file.write(dumps(employeeDict))
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,23 +3,24 @@
 from json import dumps
 import requests
 
-endpoint = "https://jsonplaceholder.typicode.com"
-users = requests.get("{}/users".format(endpoint)).json()
-users_count = len(users)
-with open('todo_all_employees.json', 'w', encoding="utf-8") as file:
-    for i in range(1, users_count):
-        employeeName = users[i].get('username')
-        todo = requests.get(
-            "{}/users/{}/todos".format(endpoint, i)).json()
-        row = []
+if __name__ == "__main__":
+    endpoint = "https://jsonplaceholder.typicode.com"
+    users = requests.get("{}/users".format(endpoint)).json()
+    users_count = len(users)
+    with open('todo_all_employees.json', 'w', encoding="utf-8") as file:
+        for i in range(1, users_count):
+            employeeName = users[i].get('username')
+            todo = requests.get(
+                "{}/users/{}/todos".format(endpoint, i)).json()
+            row = []
 
-        for elem in todo:
-            dict = {
-                "username": employeeName,
-                "task": elem.get('title'),
-                "completed": elem.get('completed')
-            }
-            row.append(dict)
-        employeeDict = {i: row}
+            for elem in todo:
+                dict = {
+                    "username": employeeName,
+                    "task": elem.get('title'),
+                    "completed": elem.get('completed')
+                }
+                row.append(dict)
+            employeeDict = {i: row}
 
-        file.write(dumps(employeeDict))
+            file.write(dumps(employeeDict))
