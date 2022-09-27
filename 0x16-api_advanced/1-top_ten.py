8843fc6044f91d1a5b593e30742f5b196836275b
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-27 09:06:54 +0000

    Add task 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""1-top_ten Module"""
+import requests
+
+
+def top_ten(subreddit):
+    if not subreddit:
+        print('None')
+    url = "https://www.reddit.com"
+    endpoint = f"/r/{subreddit}/hot.json?limit=10"
+    request = requests.get(f"{url}{endpoint}", headers={
+                           'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
+    if request.status_code == 200:
+        for elem in request.json().get('data').get('children'):
+            if elem.get('data').get('title'):
+                print(elem.get('data').get('title'))
