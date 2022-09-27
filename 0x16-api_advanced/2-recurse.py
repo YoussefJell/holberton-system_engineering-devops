bbcdca3a2030902ded3689fd8f308c8b83ffa7af
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-27 09:39:06 +0000

    Add

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""1-2-recurse Module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    if not subreddit:
+        print('None')
+    url = "https://www.reddit.com"
+    endpoint = f"/r/{subreddit}/hot.json?after={after}"
+
+    request = requests.get(f"{url}{endpoint}",
+                           headers={'User-agent': 'Mozilla/5.0'},
+                           allow_redirects=False)
+
+    if request.status_code == 200:
+        children = request.json().get('data').get('children')
+        after = request.json().get('data').get('after')
+
+        for elem in children:
+            hot_list.append(elem.get('data').get('title'))
+
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+
+    return None
